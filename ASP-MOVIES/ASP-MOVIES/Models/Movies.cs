9e63fcb0d898c69359bc500589ed6b7a95fa5dd0
Leopold Juster <leojuster16@gmail.com>
2021-12-03 02:22:56 +0000

    pages

---
+++ ASP-MOVIES/ASP-MOVIES/Models/Movies.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_MOVIES.Models
+{
+    public enum Genre
+    {
+        Comedy, Drama, Action, Thriller
+    }
+    public class Movies
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        public string Title { get; set; }
+       
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public Genre Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
